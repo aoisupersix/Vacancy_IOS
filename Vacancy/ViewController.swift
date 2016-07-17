4e96f7a629a4cbbac14b88ef5205e981aca14cc1
aoisupersix <aoi_supersix@yahoo.co.jp>
2016-07-17 18:51:09 +0900

    Initial Commit

---
+++ Vacancy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vacancy
+//
+//  Created by 田中葵 on 2016/07/17.
+//  Copyright © 2016年 田中葵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
